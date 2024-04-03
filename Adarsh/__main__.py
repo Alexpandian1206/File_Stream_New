140ac09f143f7c01c70545464a1f206f09b682bc
Illegal Developer / <147461995+IllegalDevelopers@users.noreply.github.com>
2024-04-03 15:55:27 +0530

    Update __main__.py

--- Adarsh/__main__.py
+++ Adarsh/__main__.py
@@ -11,6 +11,7 @@ from .bot import StreamBot
 from .vars import Var
 from aiohttp import web
 from .server import web_server
+from bot import app
 from .utils.keepalive import ping_server
 from Adarsh.bot.clients import initialize_clients
 
@@ -25,6 +26,7 @@ logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 ppath = "Adarsh/bot/plugins/*.py"
 files = glob.glob(ppath)
 StreamBot.start()
+app.start()
 loop = asyncio.get_event_loop()
 
 
