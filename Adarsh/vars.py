0319c419b7a0f233413e9ff7590706714860cbf9
Alex27ak <96329236+Alex27ak@users.noreply.github.com>
2024-04-12 17:43:08 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -10,21 +10,21 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', ''))
-    API_HASH = str(getenv('API_HASH', ''))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', ''))
-    FILE_BOT_TOKEN = str(getenv('FILE_BOT_TOKEN', ''))
+    API_ID = int(getenv('API_ID', '11450835'))
+    API_HASH = str(getenv('API_HASH', '0fadb61feae6ccf016932823bbf1565c'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6611868437:AAFS9g3wZBqTSMdT50EUKez5ehmQ8c-urww'))
+    FILE_BOT_TOKEN = str(getenv('FILE_BOT_TOKEN', '6600157823:AAFXUIEJZyNKfDd92v5wfBbNRYFOooMKhrE'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL','-1002025354142'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1720319665").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1254785184").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'user_is_leo'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'therialex_1206'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -32,7 +32,7 @@ class Var(object):
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL',False))
+    HAS_SSL=bool(getenv('HAS_SSL',True))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
