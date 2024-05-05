44f8784981472790c450f1a147dcba3cdd191f8b
Rishikesh Sharma(Rk_botz) <118276768+Rishikesh-Sharma09@users.noreply.github.com>
2024-05-05 16:34:57 +0530

    Update stream_routes.py

--- Adarsh/server/stream_routes.py
+++ Adarsh/server/stream_routes.py
@@ -14,7 +14,7 @@ from Adarsh.server.exceptions import FIleNotFound, InvalidHash
 from Adarsh import StartTime, __version__
 from ..utils.time_format import get_readable_time
 from ..utils.custom_dl import ByteStreamer
-from Adarsh.utils.render_template import render_page
+from Adarsh.utils.render_template import render_page, media_watch
 from Adarsh.vars import Var
 
 
@@ -52,7 +52,13 @@ async def stream_handler(request: web.Request):
             secure_hash = request.rel_url.query.get("hash")
         return web.Response(text=await render_page(id, secure_hash), content_type='text/html')
     except InvalidHash as e:
-        raise web.HTTPForbidden(text=e.message)
+        path = request.match_info["path"]
+        match = re.search(r"^([a-zA-Z0-9_-]{6})(\d+)$", path)
+        if match:
+            id = int(match.group(2))
+        else:
+            id = int(re.search(r"(\d+)(?:\/\S+)?", path).group(1))
+        return web.Response(text=await media_watch(id), content_type='text/html')
     except FIleNotFound as e:
         raise web.HTTPNotFound(text=e.message)
     except (AttributeError, BadStatusLine, ConnectionResetError):
