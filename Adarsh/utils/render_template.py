1796bf0830fe4acce6381b587bb1b11da72cc2fd
Rishikesh Sharma(Rk_botz) <118276768+Rishikesh-Sharma09@users.noreply.github.com>
2024-05-06 17:16:50 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -54,7 +54,7 @@ async def media_watch(id):
 async def batch_page(message_id_x, message_id_y):
     links_with_names = []
     for i in range(message_id_x, message_id_y + 1):
-        file_data=await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
+        file_data=await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(str(id)))
         secure_hash = file_data.unique_id[:6]
         link = urllib.parse.urljoin(Var.URL, f'{secure_hash}{str(id)}')
         file_name = file_data.file_name
