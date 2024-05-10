7dcf9ed5b9c9b438942eb5500853016011d934f5
Rishikesh Sharma(Rk_botz) <118276768+Rishikesh-Sharma09@users.noreply.github.com>
2024-05-10 14:42:33 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -61,9 +61,7 @@ async def batch_page(message_id):
     for message_id in message_ids:
         file_data=await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(message_id))
         secure_hash = file_data.unique_id[:6]
-        filename = file_data.file_name
-        strem_info = f"{message_id}/{filename}?hash={secure_hash}"
-        link = f"{Var.URL}watch/{strem_info}"
+        link = f"{Var.URL}watch/{message_id}"
         file_name = re.sub(r'[-_.]', ' ', file_data.file_name).title()
         links_with_names.append((file_name, link))
 
